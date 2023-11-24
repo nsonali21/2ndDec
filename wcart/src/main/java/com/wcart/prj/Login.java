b2547fffd78cb2c59856b2f10a19d22e8a2ef3e7
nsonali <nsonali21@gmail.com>
2023-11-24 17:18:25 +0530

    Initial change

---
+++ wcart/src/main/java/com/wcart/prj/Login.java
@@ -0,0 +1,9 @@
+package com.wcart.prj;
+
+public class Login {
+public static void main(String[] args) {
+	System.out.println("Hello 1st Git project");
+	
+	System.out.println("Hello 2nd change Git project");
+}
+}
